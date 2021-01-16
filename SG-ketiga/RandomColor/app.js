6c3516a3ead22510559d647716148c18d2a72e2a
Lucky7Tb <Luckytri101@gmail.com>
2021-01-16 23:07:24 +0700

    add slider

--- SG-ketiga/RandomColor/app.js
+++ SG-ketiga/RandomColor/app.js
@@ -1,11 +1,11 @@
 const randomColorButton = document.getElementById("random-color-button");
 const rgbColorContainer = document.getElementById("rgb-color-container");
+const rRange = document.getElementById("rRange");
+const gRange = document.getElementById("gRange");
+const bRange = document.getElementById("bRange");
 const body = document.querySelector("body");
 
-let randomColor = randomRGBColor();
-
-body.style.backgroundColor = randomColor;
-rgbColorContainer.textContent = randomColor;
+randomRGBColor();
 
 randomColorButton.addEventListener("click", () => {
 	randomColor = randomRGBColor();
@@ -13,8 +13,36 @@ randomColorButton.addEventListener("click", () => {
 	rgbColorContainer.textContent = randomColor;
 })
 
+rRange.addEventListener("mousemove", () => {
+	changeColor(rRange.value, gRange.value, bRange.value);
+})
+
+gRange.addEventListener("mousemove", () => {
+	changeColor(rRange.value, gRange.value, bRange.value);
+})
+
+bRange.addEventListener("mousemove", () => {
+	changeColor(rRange.value, gRange.value, bRange.value);
+})
+
+function changeColor(r, g, b){
+	const rgbColor = `rgb(${r}, ${g}, ${b})`;
+	body.style.backgroundColor = rgbColor;
+	rgbColorContainer.textContent = rgbColor;
+}
+
 function randomRGBColor(){
-	return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+	const rColor = randomNumber();
+	const gColor = randomNumber();
+	const bColor = randomNumber();
+	const randomColor = `rgb(${rColor}, ${gColor}, ${bColor})`;
+
+	body.style.backgroundColor = randomColor;
+	rgbColorContainer.textContent = randomColor;
+
+	rRange.value = rColor;
+	gRange.value = gColor;
+	bRange.value = bColor;
 }
 
 function randomNumber(){
